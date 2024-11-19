aa858052b2de0e334fa5e257df2c9f0f40985d8a
Manish Kumar Mourya <manishmourya108@gmail.com>
2024-11-19 19:09:59 +0000

    updated eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,22 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        require: "readonly",
+        __dirname: "readonly",
+        module: "readonly",
+      },
+    },
+    env: {
+      node: true, // Enable Node.js global variables
+    },
+    rules: {
+      "no-undef": "error", // Keep rule for other undefined variables
+    },
+  },
 ];
